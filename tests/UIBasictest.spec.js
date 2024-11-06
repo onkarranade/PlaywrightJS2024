6659536ebed619bf4cfda3d8fcd91e2524e44d59
onkarranade <onkarranade1989@gmail.com>
2024-11-06 08:39:48 -0500

    logintests file added

---
+++ tests/UIBasictest.spec.js
@@ -0,0 +1,18 @@
+const { page,test, expect} =require('@playwright/test');
+
+test('First playwright test', async ({browser,page})=>
+{
+
+await page.goto("https://google.com");
+await page.got
+
+
+});
+
+test.only('title confirmation test', async ({page})=>
+ {
+
+  await  page.goto("https://google.com");
+  await  expect(page).toHaveTitle('Google');
+
+});
---
+++ tests/loginTests.spec.js
@@ -0,0 +1,27 @@
+const { page,test, expect} =require('@playwright/test');
+
+test('incorrect login test', async ({page})=> {
+
+await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+await page.locator('#username').fill('qaonkar3@mailinator.com');
+await page.locator('#password').fill('Qa@123456');
+await page.locator('#signInBtn').click();
+await page.locator('#username').type
+
+console.log(await page.locator("div[style*='block']").textContent());
+await expect(page.locator("div[style*='block']")).toContainText('Incorrect username/password.');
+})  
+
+
+test.only('get product list', async ({page})=>{
+
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    await page.locator('#username').fill('');
+    await page.locator('#username').fill('rahulshettyacademy');
+    await page.locator('#password').fill('learning');
+    await page.locator('#signInBtn').click();
+
+    console.log(await page.locator('.card-body a').first().textContent());
+    console.log(await page.locator('.card-body a').nth(2).textContent());
+
+})
