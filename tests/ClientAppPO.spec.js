cb3b3780acc6d785a67086bfb5f2afa71c7e2a8f
onkarranade <onkarranade1989@gmail.com>
2025-01-06 08:35:14 -0500

    added test to read data from external json file. testdata.json file added

--- tests/ClientAppPO.spec.js
+++ tests/ClientAppPO.spec.js
@@ -1,15 +1,19 @@
 const { test, expect} =require('@playwright/test');
+const fs=require('fs');
+const path = require('path');
 const { LoginPage } =require('../pageobjects/LoginPage');
 const { DashboardPage } =require('../pageobjects/DashboardPage')
 const { CartPage } =require('../pageobjects/CartPage');
 const { OrderReviewPage } = require('../pageobjects/OrderReviewPage');
 const { OrderHistoryPage }=require('../pageobjects/OrderHistoryPage');
+const jsonFilePath = path.join(__dirname, '../utils/TestData.json');
+const dataset=JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
 const useremail='qaonkar7@mailinator.com';
 const password='Qa@123456';
 const productName='ADIDAS ORIGINAL';
+//console.log(data);
 
-
-test('client login test', async ({page})=> {
+test.skip('complete order test', async ({page})=> {
 
     const loginPage=new LoginPage(page);
     const dashboardPage=new DashboardPage(page);
@@ -17,10 +21,10 @@ test('client login test', async ({page})=> {
     const orderReviewPage=new OrderReviewPage(page);
     const orderHistoryPage=new OrderHistoryPage(page);
     await loginPage.goTo();
-    await loginPage.validLogin(useremail,password);
-    await dashboardPage.searchProducts(productName);
+    await loginPage.validLogin(data.useremail,data.password);
+    await dashboardPage.searchProducts(data.productName);
    await dashboardPage.navigateToCart();
-   await cartPage.verifyProductDisplayed(productName);
+   await cartPage.verifyProductDisplayed(data.productName);
     await cartPage.checkout();
     await orderReviewPage.searchCountryandSelect();
     const orderId = await orderReviewPage.SubmitAndGetOrderId();
@@ -35,3 +39,26 @@ test('client login test', async ({page})=> {
 
 
 })
+
+test.skip('sample test', async ({page})=> {
+
+    await page.goto('https://rahulshettyacademy.com/client/');
+     await page.locator('#userEmail').fill(data.useremail);
+     await page.locator('#userPassword').fill(data.password);
+     await page.locator('#login').click();
+     await page.waitForLoadState('networkidle');
+})
+
+
+for(const data of dataset)
+{
+    test(`data driven test  for ${data.useremail}`,async ({page})=> {
+        await page.goto('https://rahulshettyacademy.com/client/');
+        await page.locator('#userEmail').fill(data.useremail);
+        await page.locator('#userPassword').fill(data.password);
+        await page.locator('#login').click();
+        await page.waitForLoadState('networkidle');
+    
+    
+    })
+}
